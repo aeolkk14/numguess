e7d01965263dc8eeb552637a7e15a1d90ad2822a
aeolkk14 <jeonej0514@gmail.com>
2023-01-11 16:47:22 +0900

    feat: Make Function choice, correct, wrong

--- numguess.py
+++ numguess.py
@@ -13,16 +13,34 @@ print(f"Nice to meet you,{username}! Please be my guest!")
 guess = int(input(f"So {username}, Guess the number(1~100): "))
 print(f"Good job {username}! You picked {guess}")
 
-if guess == answer:
-    print('*********************')
-    sleep(1)
-    print('*********************')
-    sleep(1)
-    print('*********************')
-    sleep(1)
-    print("Correct! You got the right answer!")
-elif guess > answer:
-    print('Wrong! The answer is lower than you enter')
-elif guess < answer:
-    print('Wrong! The answer is higher than you enter')
 
+def choice():
+  n_guess=int(input('Choose again: '))
+  guess = n_guess
+  return guess
+
+
+def correct():
+      print('*********************')
+      sleep(1)
+      print('*********************')
+      sleep(1)
+      print('*********************')
+      sleep(1)
+      return print("Correct! You got the right answer!")
+
+def wrong():
+  if guess > answer:
+      print('Wrong! The answer is lower than you enter')
+  elif guess < answer:
+      print('Wrong! The answer is higher than you enter')
+
+
+
+for i in range(3):
+  if guess == answer:
+    correct()
+    break
+  else:
+    wrong()
+    guess = choice()    
